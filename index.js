5fee6e7e23ad9538a17fedfeb2d65f6fc05993da
adeleine1412 <53810001+adeleine1412@users.noreply.github.com>
2022-09-04 17:56:29 +0200

    Add filter icon

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
   waitForKeyElements("#sort-menu", AppendSortMenu);
 
   function AppendSortMenu() {
-    $('#sort-menu').append($.parseHTML('<a class="adeleine-sort-menu"><yt-icon id="label-icon" class="style-scope yt-dropdown-menu"><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;" class="style-scope yt-icon"><g class="style-scope yt-icon"><path d="M21,6H3V5h18V6z M15,11H3v1h12V11z M9,17H3v1h6V17z" class="style-scope yt-icon"></path></g></svg></yt-icon>Sort by</a>'));
+    $('#sort-menu').append($.parseHTML('<a class="adeleine-sort-menu"><img src="https://raw.githubusercontent.com/adeleine1412/youtube-artist-sort-menu/master/assets/filter.svg" width="24" height="24"></yt-icon>Sort by</a>'));
   }
 
 })();
