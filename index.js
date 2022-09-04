e8bfbf2a2a12c2d73cc9ffb654f7d47af39774d2
adeleine1412 <53810001+adeleine1412@users.noreply.github.com>
2022-09-04 18:09:14 +0200

    Remove jQuery

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 // @description  add the sort menu to artist channels, as it is hidden from them
 // @author       adeleine
 // @match        https://www.youtube.com/*
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @resource     IMPORT_CSS https://raw.githubusercontent.com/adeleine1412/youtube-artist-sort-menu/master/css/style.min.css
 // @grant        GM_getResourceText
@@ -18,7 +17,15 @@
   waitForKeyElements("#sort-menu", AppendSortMenu);
 
   function AppendSortMenu() {
-    $('#sort-menu').append($.parseHTML('<a class="adeleine-sort-menu"><img src="https://raw.githubusercontent.com/adeleine1412/youtube-artist-sort-menu/master/assets/filter.svg" width="24" height="24"></yt-icon>Sort by</a>'));
+    let sort_menu = document.querySelector('#sort-menu');
+    let html = document.createElement('div');
+    html.innerHTML = '\
+    <a class="adeleine-sort-menu">\
+      Sort by\
+    </a>\
+    '
+
+    if (sort_menu.innerHTML = "") sort_menu.appendChild(html);
   }
 
 })();
