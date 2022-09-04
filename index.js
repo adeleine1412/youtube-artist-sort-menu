9ed7cd861b4976dbf4e68e6fcd22498760ba4737
adeleine1412 <53810001+adeleine1412@users.noreply.github.com>
2022-09-05 00:34:11 +0200

    Switch to waitForKeyElements for better stability with ajax

--- index.js
+++ index.js
@@ -4,11 +4,13 @@
 // @version      1
 // @description  add the sort menu to artist channels, as it is hidden from them
 // @author       adeleine
-// @match        https://www.youtube.com/c/*
+// @match        https://www.youtube.com/*
 // @resource     css https://github.com/adeleine1412/youtube-artist-sort-menu/raw/master/css/style.min.css
 // @resource     html https://github.com/adeleine1412/youtube-artist-sort-menu/raw/master/html/sort-menu.html
 // @updateURL    https://github.com/adeleine1412/youtube-artist-sort-menu/raw/master/index.js
 // @downloadURL  https://github.com/adeleine1412/youtube-artist-sort-menu/raw/master/index.js
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -17,8 +19,7 @@
   'use strict';
 
   GM_addStyle(GM_getResourceText("css"));
-
-  runWhenReady("#sort-menu", AppendSortMenu);
+  waitForKeyElements("#sort-menu", AppendSortMenu);
 
   function AppendSortMenu() {
     if (document.querySelector('#sort-menu').innerHTML == "") {
@@ -32,24 +33,6 @@
         document.querySelector('.adeleine-sort-menu-wrapper>.dropdown').classList.toggle('active');
       });
     }
-  }
-
-  function runWhenReady(readySelector, callback) {
-    var numAttempts = 0;
-    var tryNow = function() {
-        var elem = document.querySelector(readySelector);
-        if (elem) {
-            callback(elem);
-        } else {
-            numAttempts++;
-            if (numAttempts >= 34) {
-                console.warn('Giving up after 34 attempts. Could not find: ' + readySelector);
-            } else {
-                setTimeout(tryNow, 250 * Math.pow(1.1, numAttempts));
-            }
-        }
-    };
-    tryNow();
   }
 
 })();
