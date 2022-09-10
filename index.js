90c9f09bef3903b837078a0c069193d6664f6bb1
adeleine1412 <53810001+adeleine1412@users.noreply.github.com>
2022-09-10 14:30:08 +0200

    drop jQuery & w.f.k.e for mutation observer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sort menu for arist channels on YouTube
 // @namespace    https://github.com/adeleine1412
-// @version      1.7
+// @version      1.8
 // @description  add the sort menu to artist channels, as it is hidden from them
 // @author       adeleine
 // @match        https://www.youtube.com/*
@@ -10,50 +10,48 @@
 // @resource     html https://github.com/adeleine1412/youtube-artist-sort-menu/raw/master/html/sort-menu.html
 // @updateURL    https://github.com/adeleine1412/youtube-artist-sort-menu/raw/master/index.js
 // @downloadURL  https://github.com/adeleine1412/youtube-artist-sort-menu/raw/master/index.js
-// @require      https://code.jquery.com/jquery-3.6.1.min.js
-// @require      https://greasyfork.org/scripts/383527-wait-for-key-elements/code/Wait_for_key_elements.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
-  // load latest styles from the github repo
+  // Load latest styles from the repository
   GM_addStyle(GM_getResourceText("css"));
 
-  // use waitForKeyElements to wait for element to be loaded, works (mostly) with ajax too
-  waitForKeyElements("#sort-menu", AppendSortMenu);
+  // Note selction and observer options
+  const targetNode = document.querySelector('body');
+  const config = { attributes: true, childList: true, subtree: true };
 
-  function AppendSortMenu() {
+  const callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
 
-    // skip if it has the default sort-menu (non-artist channels)
-    if (document.querySelector('#sort-menu').innerHTML == "") {
+      // Skip if it has the default sort-menu (non-artist channels)
+      if (document.querySelector('#sort-menu').innerHTML == "") {
 
-      // create new element and append html code from the github rebo
-      let sort_menu = document.querySelector('#sort-menu');
-      let html = document.createElement('div');
-      html.innerHTML = GM_getResourceText("html");
-      sort_menu.appendChild(html);
+        // Create new element and append html code from the github rebo
+        let sort_menu = document.querySelector('#sort-menu');
+        let html = document.createElement('div');
+        html.innerHTML = GM_getResourceText("html");
+        sort_menu.appendChild(html);
 
-      // click event to hide dropdown when clicking outside of it
-      window.addEventListener('click', function(e) {
-        document.querySelector('.adeleine-sort-menu-wrapper>.dropdown').classList.remove('active');
-      });
+        // Click event to hide dropdown when clicking outside of it
+        window.addEventListener('click', function (e) {
+          document.querySelector('.adeleine-sort-menu-wrapper>.dropdown').classList.remove('active');
+        });
 
-      // show the dropdown and stop propagation to the event above
-      document.querySelector('.adeleine-sort-menu').addEventListener('click', function(e) {
-        e.stopPropagation();
-        document.querySelector('.adeleine-sort-menu-wrapper>.dropdown').classList.toggle('active');
-      });
+        // Show the dropdown and stop propagation to the event above
+        document.querySelector('.adeleine-sort-menu').addEventListener('click', function (e) {
+          e.stopPropagation();
+          document.querySelector('.adeleine-sort-menu-wrapper>.dropdown').classList.toggle('active');
+        });
+      }
     }
+  };
 
-  }
-
-  // fallback update for very specific ajax behaviour
-  setInterval(function() {
-    if (document.querySelector('#sort-menu') != null) AppendSortMenu();
-  }, 1000);
- 
+  // Create an observer instance linked to the callback function and start observing
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
 
 })();
