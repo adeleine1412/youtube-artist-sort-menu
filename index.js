7f05c8a5fe302d38cd72838e61cf8c9c9a6d11e2
adeleine1412 <53810001+adeleine1412@users.noreply.github.com>
2022-09-26 16:16:07 +0200

    Link icon, bump version

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         Sort menu for arist channels on YouTube
 // @namespace    https://github.com/adeleine1412
-// @version      1.10.0
+// @icon         https://github.com/adeleine1412/youtube-artist-sort-menu/raw/master/img/icon.png
+// @icon64       https://github.com/adeleine1412/youtube-artist-sort-menu/raw/master/img/icon.png
+// @version      1.10.1
 // @description  add the sort menu to artist channels, as it is hidden from them
 // @author       adeleine
 // @match        https://www.youtube.com/*
